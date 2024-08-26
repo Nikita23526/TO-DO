d04bed9451fd262fde3645a3192a366ed4105e4a
NIKITA KUMARI <nkumari10344@gmail.com>
2024-08-26 11:30:03 +0530

    ADD ENV VARIABLE

--- src/config/AxiosConfig.js
+++ src/config/AxiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL="http://localhost:8080/api/v1"
+const baseURL= process.env.REACT_APP_BACKEND_API_URL ||"http://localhost:8080/api/v1"
 
 const PublicAxios = axios.create({
     baseURL: baseURL,
